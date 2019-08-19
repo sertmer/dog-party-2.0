3e6f10a9b15650120c749a7e4c4767831e3018d1
sertmer <scottertmer@gmail.com>
2019-08-18 19:04:14 -0600

    Add form reset statement and cleared commented code

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
-// function handleSubmitName(event) {
-//   console.log(event);
-//
-// }
-//
-// document.querySelector("#submit-name").addEventListener("click", function(event) {
-//   event.preventDefault()
-//   handleSubmitName(event)
-// })
-
 var dogNameInput = document.querySelector('#doggo');
 var nameThisDogButton = document.querySelector('#submit-name');
 var spanSearch = document.querySelector('span');
 
-nameThisDogButton.addEventListener('click', function(){
+nameThisDogButton.addEventListener('click', function() {
   var dogName = dogNameInput.value;
   spanSearch.innerText = dogName;
   event.preventDefault();
+  document.getElementById("my-form").reset();
 });
-
-// function nameThisDog
-//
-// document.querySelector.addEventListener('click',)
