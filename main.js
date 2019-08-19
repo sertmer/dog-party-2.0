00673ef88aa6092039d68356f7152308c79c48c4
sertmer <scottertmer@gmail.com>
2019-08-18 18:49:21 -0600

    Add working name change function

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-function handleSubmitName(event) {
-  console.log(event);
+// function handleSubmitName(event) {
+//   console.log(event);
+//
+// }
+//
+// document.querySelector("#submit-name").addEventListener("click", function(event) {
+//   event.preventDefault()
+//   handleSubmitName(event)
+// })
 
-}
+var dogNameInput = document.querySelector('#doggo');
+var nameThisDogButton = document.querySelector('#submit-name');
+var spanSearch = document.querySelector('span');
 
-document.querySelector("#submit-name").addEventListener("click", function(event) {
-  event.preventDefault()
-  handleSubmitName(event)
-})
+nameThisDogButton.addEventListener('click', function(){
+  var dogName = dogNameInput.value;
+  spanSearch.innerText = dogName;
+  event.preventDefault();
+});
+
+// function nameThisDog
+//
+// document.querySelector.addEventListener('click',)
